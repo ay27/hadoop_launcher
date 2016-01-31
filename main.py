3208fca82c6abae40aba4d5254fa1f67a0d871a7
ay27 <me@ay27.pw>
2016-01-31 19:44:04 +0800

    川气生晓夕，野阴乍烟雾。沉沉滮池水，人马不敢渡。

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ cmd_mkdir_input = 'hadoop fs -mkdir -p %s' % input_dir
 cmd_rm_output = 'hadoop fs -rm -r %s' % output_dir
 cmd_put_input_file = 'hadoop fs -put -f %s %s' % (files, input_dir)
 cmd_run_task = 'hadoop jar %s %s %s' % (jar_file, input_dir, output_dir)
+cmd_ls_output_dir = 'hadoop fs -ls %s' % output_dir
 # cmd_ls_result_dir = 'hadoop fs -ls %s' % output_dir
 # cmd_cat_result = 'hadoop fs -cat %s'
+cmd_get_result = 'hadoop fs -get %s %s'
 
 
-def runit(cmd, log_it=False):
+def run_cmd(cmd, log_it=False):
     if log_it:
         p = subprocess.Popen('%s &> %s' % (cmd, log_file), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     else:
@@ -32,29 +34,49 @@ def runit(cmd, log_it=False):
 
 def run_task(log_it=False):
     print('clean input dir and output dir')
-    runit(cmd_mkdir_input, log_it)
+    run_cmd(cmd_mkdir_input, log_it)
     try:
-        print('delete output file? (yes)')
-        line = sys.stdin.readline().strip()
-        if line.upper() == 'YES' or len(line) == 0:
-            runit(cmd_rm_output, log_it)
-        else:
-            print('output dir already exist')
-            sys.exit(-1)
+        flag = False
+        try:
+            run_cmd(cmd_ls_output_dir)
+        except AssertionError:
+            flag = True
+        if not flag:
+            print('delete output file? (yes)')
+            line = sys.stdin.readline().strip()
+            if line.upper() == 'YES' or len(line) == 0:
+                run_cmd(cmd_rm_output, log_it)
+            else:
+                print('output dir already exist')
+                sys.exit(-1)
     except AssertionError:
         pass
     print('upload input files...')
-    runit(cmd_put_input_file, log_it)
+    run_cmd(cmd_put_input_file, log_it)
     print('upload success')
 
     print('start hadoop task...')
     start_time = time.time()
-    runit(cmd_run_task, log_it)
+    run_cmd(cmd_run_task, log_it)
     print('hadoop task finish, elapsed time = %f' % (time.time() - start_time))
 
 
 def get_result():
-    print('result')
+    print('ls %s' % output_dir)
+    out = run_cmd(cmd_ls_output_dir)
+    out = out.decode()
+    out = out.split('\n')
+    out_files = []
+    for row in out:
+        words = row.split()
+        if len(words) == 8:
+            out_files.append(words[-1])
+    for (index, filePath) in enumerate(out_files):
+        if filePath.split('/')[-1].startswith('_'):
+            print(' SKIP::: [%d]. %s' % (index, filePath))
+        else:
+            print(' GET::: [%d]. %s' % (index, filePath))
+            run_cmd(cmd_get_result % (filePath, filePath.split('/')[-1]))
 
 
 def parse_args():
@@ -66,7 +88,7 @@ def parse_args():
     run_parser.add_argument('-log', action='store_const',
                             const=True, default=False, help='log file')
 
-    get_result_parser = subparser.add_parser('result', help='get result')
+    get_result_parser = subparser.add_parser('dump', help='dump result')
     get_result_parser.set_defaults(command_object=get_result)
     args = parser.parse_args()
     try:
